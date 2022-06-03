04b86af7ae14b2c987f1468ba1fecbb17acafc5e
Terezzinha <38471849+Terezzinha@users.noreply.github.com>
2022-06-03 13:56:16 -0300

    feat: inclusão das artes do jogo feat: adequado a correção da angela

--- day14_game_higher_lower.py
+++ day14_game_higher_lower.py
@@ -1,70 +1,84 @@
 
-## write this in vs code cloud on github
-## write this in my local machine
+## Display logo game
 import art
-## Print logo game
 print(art.logo_higher_lower)
 
 def format_data(f_data):
+    """Format the account data into printable format"""
     nome = f_data['name']
     follower = f_data['follower_count']
     description = f_data['description']  
     country = f_data['country']
 
-    print(f"{nome}, {description}, from {country} ")
+    return(f"{nome}, {description}, from {country} ")
 
 
 def get_followers(f_data):
+    """Get the number of follewer of Instagram account"""
     follower = f_data['follower_count']
     return follower
 
+def check_answer (user_guess, followersA, followersB):
+    """Return if user guess is correct"""
+    if followerA > followerB:
+        return user_guess == "A"
+    elif followersB > followersA:
+        return user_guess == "B"
+        
+
 # Generate a random account from the game data.
 import random
-from day14_game_data_higher_lower import data
+from day14_data_higher_lower import data
 
 diceA = random.choice(data)
 diceB = random.choice(data)
+if diceA == diceB:
+    diceB = random.choice(data)
 
-still_win = True
+is_correct = True
+score = 0
 
-while still_win:
-    # Format account data into printable format.
-    format_data(diceA)
+while is_correct:
+    print(f"Compare A: {format_data(diceA)}" ) 
     print("VS")
-    format_data(diceB)
+    print(f"Compare B: {format_data(diceB)}" ) 
     print('--------------------------------')
 
     followerA = get_followers(diceA)
     followerB = get_followers(diceB)
 
-    answer = input ("Who has mores followers on Instagram? A ou B: " )
-
-    if (answer == "A") and (followerA > followerB):
-        print("You win")
-    elif (answer == "B") and (followerA < followerB):
-        print("You win")
+    answer = input ("> Who has mores followers on Instagram? A ou B:" ).upper()
+    
+    is_correct = check_answer(answer, followerA, followerB)
+    if is_correct:
+        score += 1
+        print(f"You right! Your scores is {score}")
     else:
-        print("You lose")    
-        still_win = False
-        
+        print(f"You lose! Final score {score}")
+    
+
+    # if (answer == "A") and (followerA > followerB):
+    #     print("You win")
+    #     score
+    # elif (answer == "B") and (followerA < followerB):
+    #     print("You win")
+    # else:
+    #     print("You lose")    
+    #     still_win = False
+    
+    print(f"A has {get_followers(diceA)}")
+    print(f"B has {get_followers(diceB)}")
+   
+   
     diceA = diceB
     diceB = random.choice(data)
+        
+   
     print('--------------------------------')
-#
-# # Load game data 
 
-
-## print("oi")
 
 ## Choose a random number
- 
 
-### Pick Dice01
-### print Vs logo
-### Pick Dice02
-
-## Show Dice01
-## Show Dice02
 
 ## Ask the user who has higher number Instagram follow  
 
