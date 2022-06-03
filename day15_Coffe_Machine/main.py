652381218333f11a7fd85b25f287e0825426dd54
Terezzinha <terezinhanr@gmail.com>
2022-06-03 16:09:19 -0300

    feat: List the requirimments

---
+++ day15_Coffe_Machine/main.py
@@ -0,0 +1,30 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# TODO: 1. Ask user what he/she like
+
+# TODO: 2. Turn off the coffe machine
+
+# TODO: 3. Print report
+
+# TODO: 4. Check resources if it is sufficient
+
+# TODO: 5. Process Coins
+
+# TODO: 6. Check if the trasaction was sucefful
+
+# TODO: 7. Make Coffe
