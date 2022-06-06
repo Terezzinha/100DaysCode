a5f410f5a1f967ce5b8d924e2db49965e43fd968
Terezzinha <terezinhanr@gmail.com>
2022-06-06 13:05:07 -0300

    feat: generate report #2 - Todo1 - ok

--- day15_Coffe_Machine/main.py
+++ day15_Coffe_Machine/main.py
@@ -3,25 +3,78 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def generate_report():
+    """Shows the amount of ingredients"""
+    water_amount = resources["water"]
+    milk_amount = resources["milk"]
+    coffe_amount = resources["coffee"]
+    print(f"Water: {water_amount} \nMilk: {milk_amount} \nCoffe: {coffe_amount}")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
-# TODO: 1. Ask user what he/she like
+# Ask user what he/she like
+user_answer = "report"  # input("What would you like? :").lower()
+# print(user_answer)
+
+# Report
+if user_answer == 'report':
+    generate_report()
+
 
 # TODO: 2. Turn off the coffe machine
-
-# TODO: 3. Print report
-
+# Seguir o caminho
 # TODO: 4. Check resources if it is sufficient
+
+
+def check_is_sufficient(order):
+    print(f'--------{order}-----------')
+
+    resources_ammount = resources
+    print(f"resources: {resources_ammount} ")
+
+    order_ingredients = MENU[order]
+    print(f"Ingredient for {order}: {order_ingredients}")
+
+    print(resources_ammount["water"])
+    print(order_ingredients["ingredients"]["water"])
+    #print(order_ingredients["ingredients"]["latte"])
+    print("\n")
+
+check_is_sufficient('espresso')
+check_is_sufficient('latte')
+check_is_sufficient('cappuccino')
 
 # TODO: 5. Process Coins
 
