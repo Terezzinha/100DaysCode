0c55fd4189dbff2e204ee88d01526e866dfb26e8
Terezzinha <38471849+Terezzinha@users.noreply.github.com>
2022-06-13 13:52:16 -0300

    chore: quiz game incialmente concluído

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+if (answer == "A") and (followerA > followerB):
+        print("You win")
+        score
+    elif (answer == "B") and (followerA < followerB):
+        print("You win")
+    else:
+        print("You lose")    
+        still_win = False
