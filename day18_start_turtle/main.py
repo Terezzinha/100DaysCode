b40661c992609940cf5249289fa36d740cdd7ccf
Terezzinha <38471849+Terezzinha@users.noreply.github.com>
2022-06-14 14:53:41 -0300

    feat: desenha polígonos

--- day18_start_turtle/main.py
+++ day18_start_turtle/main.py
@@ -1,23 +1,27 @@
 from turtle import Turtle, Screen
-import heroes
 
-
-#timmy_the_turtle = Turtle()
-#timmy_the_turtle.shape("circle")
-#timmy_the_turtle.color("red", "green")
 
 def draw_line():
     timmy_the_turtle.forward(100)
     timmy_the_turtle.left(90)
 
 
+def draw_polygon(num_sides):
+    angle = 360 / num_sides
+    for i in range(num_sides):
+        timmy_the_turtle.right(angle)
+        timmy_the_turtle.forward(100)
+
+
 timmy_the_turtle = Turtle()
 timmy_the_turtle.shape("circle")
 timmy_the_turtle.color("blue")
 
-for _ in range(4):
-    draw_line()
+for i in range(3, 8):
+    draw_polygon(i)
 
 
 screen = Screen()
 screen.exitonclick()
+
+
