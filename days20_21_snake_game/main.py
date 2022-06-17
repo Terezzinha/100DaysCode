ab97c29093851f853b8f7d187069f127a1b4891c
Terezzinha <38471849+Terezzinha@users.noreply.github.com>
2022-06-17 14:51:08 -0300

    feat: inclui controles de posição

--- days20_21_snake_game/main.py
+++ days20_21_snake_game/main.py
@@ -1,20 +1,27 @@
 import random
 import turtle as t
+from snake import Snake
+import time
 
 screen = t.Screen()
-
-
-def create_snake_body():
-    starting_position = [(0, 0), (-20,0), (-40, 0)]
-    for position in starting_position:
-        new_segment = t.Turtle("square")
-        new_segment.color("white")
-        new_segment.goto(position)
-
-
-create_snake_body()
-
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+screen.tracer(0)
+
+sucuri = Snake()
+
+screen.listen()
+screen.onkey(sucuri.up, "Up")
+screen.onkey(sucuri.down, "Down")
+screen.onkey(sucuri.left, "Left")
+screen.onkey(sucuri.right, "Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    sucuri.move()
+
+
 screen.exitonclick()
