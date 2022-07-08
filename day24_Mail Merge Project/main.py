0106f88631db45a0751a31dc08c503a5badad209
Terezzinha <38471849+Terezzinha@users.noreply.github.com>
2022-07-08 15:12:01 -0300

    task: define as tarefas da aplicação

---
+++ day24_Mail Merge Project/main.py
@@ -0,0 +1,7 @@
+# TODO: abrir os arquivos de nomes
+
+# TODO: cortar os nomes em uma lista
+
+# TODO: percorrer a lista e substuir o nome na carta
+
+# TODO: criar um novo arquivo com o nome da pessoa na pasta ReadyToSend
