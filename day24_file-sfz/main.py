5699b3b45ba727d0bb5e056f3434ed1fbadfab2f
Terezzinha <38471849+Terezzinha@users.noreply.github.com>
2022-07-08 14:57:39 -0300

    feat: arquivos preparatório da mala direta

--- day24_file-sfz/main.py
+++ day24_file-sfz/main.py
@@ -5,5 +5,10 @@
 #with open("meu_arquivo.txt", mode="a") as file:
 #    file.write("\nlinha 2")
 
-with open("criar_arquivo.txt", mode="w") as file:
-    file.write("\nlinha q")
+#with open("criar_arquivo.txt", mode="w") as file:
+#    file.write("\nlinha q")
+
+#Paths
+with open("../../arquivos_auxiliares/estudo_path_py.txt") as file:
+    contents = file.read()
+    print(contents)
