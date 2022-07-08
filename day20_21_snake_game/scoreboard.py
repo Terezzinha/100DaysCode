6f2d9f130b4090f504b29cf494294d11125373c3
Terezzinha <38471849+Terezzinha@users.noreply.github.com>
2022-07-08 14:31:38 -0300

    feat: inclui registro de high_score

--- day20_21_snake_game/scoreboard.py
+++ day20_21_snake_game/scoreboard.py
@@ -7,6 +7,8 @@ class Scoreboar(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt", mode="r") as score_file:
+            self.high_score = int(score_file.read())
         self.color("white")
         self.penup()
         self.goto(0,270)
@@ -14,7 +16,16 @@ class Scoreboar(Turtle):
         self.update_scoreboar()
 
     def update_scoreboar(self):
-        self.write(f"Score {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as score_file:
+                data.write(str(self.high_score))
+        self.score = 0
+        self.update_scoreboar()
 
     def game_over(self):
         self.goto(0, 0)
@@ -22,6 +33,10 @@ class Scoreboar(Turtle):
 
     def increase_score(self):
         self.score += 1
+        new_score = int(self.high_score) + 1
+        with open("data.txt", mode="w") as score_file:
+            score_file.write(str(new_score))
+
         self.clear()
         self.update_scoreboar()
 
