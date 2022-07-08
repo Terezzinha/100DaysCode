5b3693259ec1b6b7c12444ca736bb69e5530906b
Terezzinha <38471849+Terezzinha@users.noreply.github.com>
2022-07-08 14:32:03 -0300

    feat: snippes de mode de tratamento de arquivo

---
+++ day24_file-sfz/main.py
@@ -0,0 +1,9 @@
+#with open("meu_arquivo.txt") as file:
+#    contents = file.read()
+#    print(contents)
+
+#with open("meu_arquivo.txt", mode="a") as file:
+#    file.write("\nlinha 2")
+
+with open("criar_arquivo.txt", mode="w") as file:
+    file.write("\nlinha q")
