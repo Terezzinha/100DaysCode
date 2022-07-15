9ab09d5a772908e2db8cbcbc502b60f0bfb43bd2
Terezzinha <terezinhanr@gmail.com>
2022-07-15 20:28:55 -0300

    feat: básico sobre manipalação de dataframe

--- day25_csv/main.py
+++ day25_csv/main.py
@@ -5,6 +5,18 @@ FUR_COLOR_01 = 'Primary Fur Color'
 data = pd.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
 
 print(data[FUR_COLOR_01].unique())
-fur_gray = (data['Primary Fur Color'] == 'Gray')
-print(data['Primary Fur Color'].value_counts())
-print(data['Primary Fur Color'].size)
+fur_gray = len(data[data['Primary Fur Color'] == 'Gray'])
+color_total = data['Primary Fur Color'].value_counts()
+print(color_total)
+
+gray = len(data[data['Primary Fur Color'] == 'Gray'])
+black = len(data[data['Primary Fur Color'] == 'Black'])
+cinnamon = len(data[data['Primary Fur Color'] == 'Cinnamon'])
+
+color_dict = {
+    "Fur color": ['Gray', 'Black', 'Cinnamon'],
+    "Count": [gray, black, cinnamon]
+}
+
+df_color = pd.DataFrame(color_dict)
+df_color.to_csv("color_fur.csv")
