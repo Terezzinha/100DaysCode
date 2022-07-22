9c9769ecc9a470e608e464b7e56fdcebef2fe2a8
Terezzinha <terezinhanr@gmail.com>
2022-07-22 19:12:28 -0300

    feat: funcionamento básico, ler e escrever o nome do estado

--- day25_states_map_game/main.py
+++ day25_states_map_game/main.py
@@ -1,18 +1,32 @@
 import turtle as tu
-FONT = ("Courier", 24, "normal")
+import pandas as pd
+
+FONT = ("Courier", 12, "normal")
 
 screen = tu.Screen()
 screen.title = "U.S States Games"
 screen.bgpic("blank_states_img.gif")
 
+ask_state = screen.textinput("Where", "State:").capitalize()
 
-def aks_the_state():
-    new_state = screen.textinput("NIM", "Name of first player:")
-    tu.write(new_state, align="left", font=FONT)
+data = pd.read_csv("50_states.csv")
+pos_x = int(data[data.state == ask_state].x)
+pos_y = int(data[data.state == ask_state].y)
+tu.penup()
+tu.goto(pos_x, pos_y)
+
+tu.write(ask_state, align="left", font=FONT)
+#
+# def aks_the_state():
+#
+# ask_state = input("Estado: ").capitalize()
+#
+# print(data[data.state == ask_state])
+#pos_x
+#pos_y = data[data.state == ask_state].y
+# print(f"pos_y {int(pos_x)} e {int(pos_y)}")
 
 
-
-# TODO criar uma classe screen_map
 
 screen.exitonclick()
 
