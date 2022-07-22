52f655876e8e7071b898a30f4721197fea710529
Terezzinha <terezinhanr@gmail.com>
2022-07-22 16:36:44 -0300

    feat: pergunta o estado e escreve na tela

--- day25_states_map_game/main.py
+++ day25_states_map_game/main.py
@@ -1,6 +1,19 @@
 import turtle as tu
+FONT = ("Courier", 24, "normal")
 
 screen = tu.Screen()
 screen.title = "U.S States Games"
+screen.bgpic("blank_states_img.gif")
+
+
+def aks_the_state():
+    new_state = screen.textinput("NIM", "Name of first player:")
+    tu.write(new_state, align="left", font=FONT)
+
+
+
+# TODO criar uma classe screen_map
+
+screen.exitonclick()
 
 
