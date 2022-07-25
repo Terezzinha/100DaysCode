518ce99a778aa9f37c480fef0e5ef02946cc16dc
Terezzinha <terezinhanr@gmail.com>
2022-07-25 19:15:22 -0300

    feat: alterando a answer_state para melhor acompanhar Angela

--- day25_states_map_game/main.py
+++ day25_states_map_game/main.py
@@ -9,24 +9,21 @@ screen = tu.Screen()
 screen.title("US STATES GAMES")
 screen.bgpic("blank_states_img.gif")
 
-def get_mouse_click_corr(x,y):
-    print(x,y)
-
 data = pd.read_csv("50_states.csv")
-ask_state = screen.textinput("Where", "State:").capitalize()
-print(data.[data.state == ask_state].values)
+answer_state = screen.textinput("Where", "State:").capitalize()
+print(data.[data.state == answer_state].values)
 
 
 game_is_on = False
 while game_is_on:
 
-    if not (data[data.state == ask_state]):
-        pos_x = int(data[data.state == ask_state].x)
-        pos_y = int(data[data.state == ask_state].y)
+    if not (data[data.state == answer_state]):
+        pos_x = int(data[data.state == answer_state].x)
+        pos_y = int(data[data.state == answer_state].y)
         tu.penup()
         tu.goto(pos_x, pos_y)
 
-        tu.write(ask_state, align="left", font=FONT)
+        tu.write(answer_state, align="left", font=FONT)
     else:
         game_is_on = False
 screen.exitonclick()
