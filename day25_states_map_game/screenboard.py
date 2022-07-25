c3555c531c9cccea0ad8fa78dd45fc104ae767fd
Terezzinha <38471849+Terezzinha@users.noreply.github.com>
2022-07-25 13:45:03 -0300

    feat: cria a classe de screenboard

---
+++ day25_states_map_game/screenboard.py
@@ -0,0 +1,9 @@
+from turtle import Turtle
+
+FONT = ("Courier", 12, "normal")
+
+class Screenboad(Turtle):
+
+    def __init__(self):
+        self.title = "U.S States Games"
+        self.bgpic("blank_states_img.gif")
