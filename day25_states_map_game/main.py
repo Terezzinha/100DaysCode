9fa3fbb25e0dcf11a334f9594df1b8ad5a26087b
Terezzinha <terezinhanr@gmail.com>
2022-07-25 19:39:23 -0300

    feat: inclui o placar

--- day25_states_map_game/main.py
+++ day25_states_map_game/main.py
@@ -1,7 +1,8 @@
 import turtle as tu
 import pandas as pd
 import timeit as ti
-
+#todo record de correct in a list
+#todo keep track of the score
 
 FONT = ("Courier", 12, "normal")
 
@@ -10,22 +11,35 @@ screen.title("US STATES GAMES")
 screen.bgpic("blank_states_img.gif")
 
 data = pd.read_csv("50_states.csv")
-answer_state = screen.textinput("Where", "State:").capitalize()
-print(data.[data.state == answer_state].values)
+
+guessed_state = []
+
+while len(guessed_state) < 4:
+    answer_state = screen.textinput(f"{len(guessed_state)}/50 States Correct", "State:").capitalize()
+    guessed_state.append(answer_state)
+
+    pos_x = int(data[data.state == answer_state].x)
+    pos_y = int(data[data.state == answer_state].y)
+    tu.penup()
+    tu.goto(pos_x, pos_y)
+
+    tu.write(answer_state, align="left", font=FONT)
 
 
-game_is_on = False
-while game_is_on:
-
-    if not (data[data.state == answer_state]):
-        pos_x = int(data[data.state == answer_state].x)
-        pos_y = int(data[data.state == answer_state].y)
-        tu.penup()
-        tu.goto(pos_x, pos_y)
-
-        tu.write(answer_state, align="left", font=FONT)
-    else:
-        game_is_on = False
+#
+#
+# game_is_on = True
+# while game_is_on:
+#
+#     if not (data[data.state == answer_state]):
+#         pos_x = int(data[data.state == answer_state].x)
+#         pos_y = int(data[data.state == answer_state].y)
+#         tu.penup()
+#         tu.goto(pos_x, pos_y)
+#
+#         tu.write(answer_state, align="left", font=FONT)
+#     else:
+#         game_is_on = False
 screen.exitonclick()
 
 
