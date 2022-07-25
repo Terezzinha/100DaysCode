b629ac547d49da317f005ad6eee632a039c12de0
Terezzinha <terezinhanr@gmail.com>
2022-07-25 19:12:26 -0300

    feat: iniciando a correção da Angela

--- day25_states_map_game/main.py
+++ day25_states_map_game/main.py
@@ -1,36 +1,34 @@
 import turtle as tu
 import pandas as pd
-import screenboard as sb
+import timeit as ti
+
 
 FONT = ("Courier", 12, "normal")
 
-screenboard = sb.Screenboad()
-
 screen = tu.Screen()
-screen.title = "U.S States Games"
+screen.title("US STATES GAMES")
 screen.bgpic("blank_states_img.gif")
 
-ask_state = screen.textinput("Where", "State:").capitalize()
+def get_mouse_click_corr(x,y):
+    print(x,y)
 
 data = pd.read_csv("50_states.csv")
-pos_x = int(data[data.state == ask_state].x)
-pos_y = int(data[data.state == ask_state].y)
-tu.penup()
-tu.goto(pos_x, pos_y)
-
-tu.write(ask_state, align="left", font=FONT)
-#
-# def aks_the_state():
-#
-# ask_state = input("Estado: ").capitalize()
-#
-# print(data[data.state == ask_state])
-#pos_x
-#pos_y = data[data.state == ask_state].y
-# print(f"pos_y {int(pos_x)} e {int(pos_y)}")
+ask_state = screen.textinput("Where", "State:").capitalize()
+print(data.[data.state == ask_state].values)
 
 
+game_is_on = False
+while game_is_on:
 
+    if not (data[data.state == ask_state]):
+        pos_x = int(data[data.state == ask_state].x)
+        pos_y = int(data[data.state == ask_state].y)
+        tu.penup()
+        tu.goto(pos_x, pos_y)
+
+        tu.write(ask_state, align="left", font=FONT)
+    else:
+        game_is_on = False
 screen.exitonclick()
 
 
