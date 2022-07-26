6fd0da4c202788b7653c28bb966d9cbb9a472f1b
Terezzinha <terezinhanr@gmail.com>
2022-07-26 20:27:49 -0300

    feat: cria arquivo csv dos estados com erro de ortografia

--- day25_states_map_game/main.py
+++ day25_states_map_game/main.py
@@ -1,5 +1,6 @@
 import turtle as tu
 import pandas as pd
+import csv
 
 # todo keep track of the score
 
@@ -14,6 +15,7 @@ all_states_lst = data.state.to_list()
 
 guessed_state = []
 count_answers = 0
+states_to_learn_lst = []
 
 while count_answers < 50:
     count_answers = len(guessed_state)
@@ -29,6 +31,13 @@ while count_answers < 50:
         tu.goto(pos_x, pos_y)
 
         tu.write(answer_state, align="left", font=FONT)
+    else:
+        states_to_learn_lst.append(str(answer_state))
 
 with open("score.txt", mode="w") as score_file:
     score_file.write(str(count_answers))
+
+states_to_learn_df = pd.DataFrame (states_to_learn_lst, columns = ['states_to_learn'])
+states_to_learn_df.to_csv("states_to_learn_fl.csv")
+# with open("states_to_learn.csv", mode="w", newline='') as states_to_learn_fl:
+#     states_to_learn_fl.writerow(states_to_learn_lst)
