aef48e0402dd0057aaddbadd35448987cad16dac
Terezzinha <terezinhanr@gmail.com>
2022-07-26 16:32:54 -0300

    feat: cria lista com todos os estados

--- day25_states_map_game/main.py
+++ day25_states_map_game/main.py
@@ -1,8 +1,8 @@
 import turtle as tu
 import pandas as pd
-import timeit as ti
-#todo record de correct in a list
-#todo keep track of the score
+
+# todo record de correct in a list
+# todo keep track of the score
 
 FONT = ("Courier", 12, "normal")
 
@@ -11,22 +11,25 @@ screen.title("US STATES GAMES")
 screen.bgpic("blank_states_img.gif")
 
 data = pd.read_csv("50_states.csv")
+all_states_lst = data.state.to_list()
 
 guessed_state = []
 count_answers = 0
 
-while count_answers < 4:
+while count_answers < 50:
     count_answers = len(guessed_state)
 
     answer_state = screen.textinput(f"{count_answers}/50 States Correct", "State:").capitalize()
-    guessed_state.append(answer_state)
+    if answer_state == "Exit":
+        break
+    if answer_state in all_states_lst:
+        guessed_state.append(answer_state)
+        pos_x = int(data[data.state == answer_state].x)
+        pos_y = int(data[data.state == answer_state].y)
+        tu.penup()
+        tu.goto(pos_x, pos_y)
 
-    pos_x = int(data[data.state == answer_state].x)
-    pos_y = int(data[data.state == answer_state].y)
-    tu.penup()
-    tu.goto(pos_x, pos_y)
-
-    tu.write(answer_state, align="left", font=FONT)
+        tu.write(answer_state, align="left", font=FONT)
 
 with open("score.txt", mode="w") as score_file:
     score_file.write(str(count_answers))
@@ -46,5 +49,3 @@ with open("score.txt", mode="w") as score_file:
 #     else:
 #         game_is_on = False
 screen.exitonclick()
-
-
