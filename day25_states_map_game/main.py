1150edc9bbbcc76fa5dbc478f5fba1725cf9be99
Terezzinha <terezinhanr@gmail.com>
2022-07-26 15:58:21 -0300

    feat: guarda as resposta certas

--- day25_states_map_game/main.py
+++ day25_states_map_game/main.py
@@ -13,9 +13,12 @@ screen.bgpic("blank_states_img.gif")
 data = pd.read_csv("50_states.csv")
 
 guessed_state = []
+count_answers = 0
 
-while len(guessed_state) < 4:
-    answer_state = screen.textinput(f"{len(guessed_state)}/50 States Correct", "State:").capitalize()
+while count_answers < 4:
+    count_answers = len(guessed_state)
+
+    answer_state = screen.textinput(f"{count_answers}/50 States Correct", "State:").capitalize()
     guessed_state.append(answer_state)
 
     pos_x = int(data[data.state == answer_state].x)
@@ -25,6 +28,8 @@ while len(guessed_state) < 4:
 
     tu.write(answer_state, align="left", font=FONT)
 
+with open("score.txt", mode="w") as score_file:
+    score_file.write(str(count_answers))
 
 #
 #
